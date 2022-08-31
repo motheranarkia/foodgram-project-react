caebf9644f3ceec5d5614b2420f3d58c6ca1b26e
motheranarkia <96246181+motheranarkia@users.noreply.github.com>
2022-08-31 04:45:40 +0300

    deploy

--- backend/recipes/migrations/0002_auto_20220826_1742.py
+++ backend/recipes/migrations/0002_auto_20220826_1742.py
@@ -18,41 +18,72 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='shoppingcart',
             name='user',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='shopping_cart', to=settings.AUTH_USER_MODEL, verbose_name='Пользователь'),
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name='shopping_cart', to=settings.AUTH_USER_MODEL,
+                verbose_name='Пользователь'
+            ),
         ),
         migrations.AddField(
             model_name='recipe',
             name='author',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='recipes', to=settings.AUTH_USER_MODEL, verbose_name='Автор рецепта'),
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name='recipes',
+                to=settings.AUTH_USER_MODEL,
+                verbose_name='Автор рецепта'
+            ),
         ),
         migrations.AddField(
             model_name='recipe',
             name='ingredients',
-            field=models.ManyToManyField(through='recipes.IngredientList', to='recipes.Ingredient', verbose_name='Список ингредиентов'),
+            field=models.ManyToManyField(
+                through='recipes.IngredientList',
+                to='recipes.Ingredient',
+                verbose_name='Список ингредиентов'
+            ),
         ),
         migrations.AddField(
             model_name='recipe',
             name='tags',
-            field=models.ManyToManyField(to='recipes.Tag', verbose_name='Теги'),
+            field=models.ManyToManyField(
+                to='recipes.Tag', verbose_name='Теги'
+            ),
         ),
         migrations.AddField(
             model_name='ingredientlist',
             name='ingredient',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='ingredient_list', to='recipes.Ingredient', verbose_name='Ингредиент'),
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name='ingredient_list', to='recipes.Ingredient',
+                verbose_name='Ингредиент'
+            ),
         ),
         migrations.AddField(
             model_name='ingredientlist',
             name='recipe',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='ingredient_list', to='recipes.Recipe', verbose_name='Рецепт'),
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name='ingredient_list', to='recipes.Recipe',
+                verbose_name='Рецепт'
+            ),
         ),
         migrations.AddField(
             model_name='favorite',
             name='recipe',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='favorite_recipe', to='recipes.Recipe', verbose_name='Рецепт'),
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name='favorite_recipe', to='recipes.Recipe',
+                verbose_name='Рецепт'
+            ),
         ),
         migrations.AddField(
             model_name='favorite',
             name='user',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='favorite_recipe', to=settings.AUTH_USER_MODEL, verbose_name='Пользователь'),
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name='favorite_recipe', to=settings.AUTH_USER_MODEL,
+                verbose_name='Пользователь'
+            ),
         ),
     ]
