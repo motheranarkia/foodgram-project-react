86f4d6b8e4eb1adf1b359ea8851c9f43c1b0c7bd
motheranarkia <96246181+motheranarkia@users.noreply.github.com>
2022-08-31 04:50:55 +0300

    deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,7 +86,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'ENGINE': os.getenv(
+            'DB_ENGINE',
+            default='django.db.backends.postgresql'
+        ),
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
