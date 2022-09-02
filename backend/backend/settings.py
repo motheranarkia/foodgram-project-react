2043274bdbceec81852c492af92d817def47f3d0
motheranarkia <96246181+motheranarkia@users.noreply.github.com>
2022-09-02 10:33:34 +0300

    change token

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -168,9 +168,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 6,
-
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ],
 }
 
