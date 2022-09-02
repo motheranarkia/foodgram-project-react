9216ef19605cb96c4d4abe9e39def22c4e028eb2
motheranarkia <96246181+motheranarkia@users.noreply.github.com>
2022-09-02 11:04:31 +0300

    change permission

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -168,11 +168,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 6,
+
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
 }
 
