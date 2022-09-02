8f2b9aaa7ae441e4356820d751221be86942c9dd
motheranarkia <96246181+motheranarkia@users.noreply.github.com>
2022-09-02 10:14:24 +0300

    token

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -168,6 +168,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 6,
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
