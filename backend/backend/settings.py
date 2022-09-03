19b09b9f5642e160d61e790744851b870372cae4
motheranarkia <96246181+motheranarkia@users.noreply.github.com>
2022-09-03 19:37:10 +0300

    change settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -173,8 +173,11 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ]
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
