da76c01faf64edfe5480def8d46923fdbd7f3f6f
motheranarkia <96246181+motheranarkia@users.noreply.github.com>
2022-09-04 16:28:04 +0300

    correction 35

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -32,7 +32,7 @@ class RecipeFilter(FilterSet):
             )
         return queryset
 
-    def filter_is_in_shopping_cart(self, queryset, value):
+    def filter_is_in_shopping_cart(self, queryset, name, value):
         if value:
             return queryset.filter(
                 shopping_cart__user=self.request.user
